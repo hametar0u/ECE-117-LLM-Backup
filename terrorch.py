d3557d41b7520fe823847f8201dc86c9567f5145
Aikawa Mafuyu <76571014+AikawaMafuyu@users.noreply.github.com>
2023-01-26 13:17:07 -0500

    Create terrorch.py

---
+++ terrorch.py
@@ -0,0 +1,42 @@
+import torch
+import torch.nn as nn
+
+#to-do: implement for 64-bit double precision
+#to-do: implement for training
+#to-do: implement sparse mat for large models
+
+class Injector():
+  def __init__(self, *args, **kwargs) -> None:
+    self.p = kwargs.get('p', 1e-10)
+    self.dtype = kwargs.get('dtype', torch.float)
+    self.size = kwargs.get('max_size')
+    self.param_names = kwargs.get('param_names')
+    self._error_map_generate()
+  
+  def _error_map_generate(self) -> None:
+    
+    self._error_map = torch.ones((self.size, torch.finfo(self.dtype).bits))
+    self._error_map = (2 * torch.ones(32, dtype = torch.int)) ** torch.arange(0, 32, dtype = torch.int).expand_as(self._error_map)
+    filter = nn.functional.dropout(torch.ones_like(self._error_map, dtype = torch.float), 1 - self.p)
+    self._error_map = filter.int() * self._error_map 
+    self._error_map = self._error_map.sum(dim = -1).int()
+
+  def inject(self, model: nn.Module) -> None:
+    for param_name, param in model.named_parameters():
+      if param_name.split('.')[-1] in self.param_names:
+        if param.numel() > self._error_map.numel():
+          raise ValueError('Your predefined error map is too small!')
+        error_mask = self._error_map[torch.randperm(self._error_map.numel())][:param.numel()]
+        error_mask = error_mask.reshape_as(param) #check
+        print(param[0][0])
+        param.data = (param.view(torch.int) ^ error_mask).view(torch.float)
+        print('--------------------')
+        print(param[0][0])
+  
+  def inject_iterate(self, model: nn.Module) -> None:
+    #to-do
+    return NotImplementedError('Iterative error injection is not implemented yet.')
+  
+  def inject_sparse(self, model: nn.Module) -> None:
+    #to-do
+    return NotImplementedError('Sparse error map is not implemented yet.')

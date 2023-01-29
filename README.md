bfb4e6b0a4a0f5a813376300d5b04d0d327571a7
AikawaMafuyu <dma@villanova.edu>
2023-01-28 20:25:44 -0500

    Working on Stuck-at-Fault implementation

--- README.md
+++ README.md
@@ -5,9 +5,10 @@
 ## To-do:
 - [X] Allow users to determine the device of generating and injecting errors.
 - [X] Automatic error map size allocation by iterating through model.
-- [ ] Support for 64-bit double precision.
 - [X] Support for save and load of error maps with option to save as sparse tensor.
 - [ ] Implement for stuck-at-fault error (with customized error map).
+- [ ] Support for random value error.
+- [ ] Support for 64-bit double precision.
 - [ ] Support error injection during training.
 - [ ] Support for quantized data types.
 ## Contact
--- terrorch/terrorch.py
+++ terrorch/terrorch.py
@@ -18,6 +18,13 @@ class Injector():
     if self.device is None:
       raise ValueError('Please specify device for error injection.')
   
+  def _errormap_size_detect(self, model: nn.Module) -> None:
+    self.maxsize = 0
+    for param_name, param in model.named_parameters():
+      if param_name.split('.')[-1] in self.param_names:
+        if param.numel() * torch.finfo(self.dtype).bits > self.maxsize:
+          self.maxsize = param.numel() * torch.finfo(self.dtype).bits
+
   def _error_map_generate(self) -> None:
     self._error_map = torch.ones((self.maxsize, torch.finfo(self.dtype).bits), device = self.device)
     self._error_map = (2 * torch.ones(32, dtype = torch.int, device = self.device)) ** torch.arange(0, 32, dtype = torch.int, device = self.device).expand_as(self._error_map)
@@ -33,13 +40,6 @@ class Injector():
         error_mask = self._error_map[torch.randperm(self._error_map.numel(), device = self.device)][:param.numel()]
         error_mask = error_mask.reshape_as(param)
         param.data = (param.view(torch.int) ^ error_mask).view(torch.float)
-  
-  def _errormap_size_detect(self, model: nn.Module) -> None:
-    self.maxsize = 0
-    for param_name, param in model.named_parameters():
-      if param_name.split('.')[-1] in self.param_names:
-        if param.numel() * torch.finfo(self.dtype).bits > self.maxsize:
-          self.maxsize = param.numel() * torch.finfo(self.dtype).bits
 
   def save_errormap(self, path, sparse = False) -> None:
     maptensor = self._error_map.clone()  
@@ -56,3 +56,41 @@ class Injector():
     if sparse == True:
       maptensor = maptensor.to_dense()
     self._error_map = maptensor.clone()
+
+# class InjectorStuckAtFault(Injector):
+#   valid_dtypes = [torch.float, ]
+
+#   def __init__(self, *args, **kwargs) -> None:
+#     super().__init__(*args, **kwargs)
+#     self._error_maps = {}
+#     raise Warning('Error injection for stuck-at-fault errors is extremely memory-intensive and thus not recommended to run on GPU!')
+  
+#   def _error_map_generate(self, model: nn.Module) -> None:
+#     for param_name, param in model.named_parameters():
+#       if param_name.split('.')[-1] in self.param_names:
+#         self._error_map['param_name'] = torch.ones((*param.shape, torch.finfo(self.dtype).bits))
+
+#   def inject(self, model: nn.Module) -> None:
+#     self._errormap_size_detect(model)
+#     self._error_map_generate()
+#     for param_name, param in model.named_parameters():
+#       if param_name.split('.')[-1] in self.param_names:
+#         error_mask = self._error_map[torch.randperm(self._error_map.numel(), device = self.device)][:param.numel()]
+#         error_mask = error_mask.reshape_as(param)
+#         param.data = (param.view(torch.int) ^ error_mask).view(torch.float)
+  
+#   def save_errormap(self, path, sparse = False) -> None:
+#     maptensor = self._error_maps.copy()
+#     if self.device != torch.device('cpu'):
+#       maptensor = [error_map.cpu() for error_map in maptensor]
+#     if sparse == True:
+#       maptensor = [error_map.to_sparse()  for error_map in maptensor]
+#     torch.save(maptensor, path)
+  
+#   def load_errormap(self, path, sparse = False) -> None:
+#     maptensor = torch.load(path)
+#     if self.device != torch.device('cpu'):
+#       maptensor = maptensor.to(self.device)
+#     if sparse == True:
+#       maptensor = maptensor.to_dense()
+#     self._error_map = maptensor.clone()

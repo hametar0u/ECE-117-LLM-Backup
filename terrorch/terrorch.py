62fc717515944873d9468ed4025adce9a37de13e
AikawaMafuyu <dma@villanova.edu>
2023-01-29 21:07:02 -0500

    Huge rework on error map generation

--- terrorch/terrorch.py
+++ terrorch/terrorch.py
@@ -5,14 +5,12 @@ class Injector():
   valid_dtypes = [torch.float, ]
   valid_error_models = ['bit', 'value']
 
-  # @classmethod
-  # def _error_map_tensor(t: torch.Tensor, error_type = 'bit') -> torch.Tensor:
-  #   if error_type == 'bit':
-  #     self._error_map = (2 * torch.ones(32, dtype = torch.int, device = self.device)) ** torch.arange(0, 32, dtype = torch.int, device = self.device).expand_as(self._error_map)
-  #     filter = nn.functional.dropout(torch.ones_like(self._error_map, dtype = torch.float, device = self.device), 1 - self.p)
-  #     self._error_map = filter.int() * self._error_map 
-  #     self._error_map = self._error_map.sum(dim = -1).int()
-  #   elif error_type == 'random_value':
+  @classmethod
+  def _error_map_generate(injectee_shape, dtype_bitwidth, device, p) -> torch.Tensor:
+    error_map = (2 * torch.ones((injectee_shape, dtype_bitwidth), dtype = torch.int, device = device)) ** torch.arange(0, dtype_bitwidth, dtype = torch.int, device = device).expand((injectee_shape, dtype_bitwidth))
+    filter = nn.functional.dropout(torch.ones_like(error_map , dtype = torch.float, device = device), 1 - p)
+    error_map  = (filter.int() * error_map).sum(dim = -1).int()
+    return error_map
 
   def __init__(self, 
       p: float = 1e-10, 
@@ -61,35 +59,32 @@ class Injector():
           self._error_maps[param_name] = Injector._error_map_generate(injectee_shape, self._dtype_bitwidth, self.device, self.p)
       raise Warning('Stuck-at-fault error injection is extremely memory-intensive. Use with caution!')
 
-  @classmethod
-  def _error_map_generate(injectee_shape, dtype_bitwidth, device, p) -> torch.Tensor:
-    error_map = (2 * torch.ones((injectee_shape, dtype_bitwidth), dtype = torch.int, device = device)) ** torch.arange(0, dtype_bitwidth, dtype = torch.int, device = device).expand((injectee_shape, dtype_bitwidth))
-    filter = nn.functional.dropout(torch.ones_like(error_map , dtype = torch.float, device = device), 1 - p)
-    error_map  = (filter.int() * error_map).sum(dim = -1).int()
-    return error_map
-
   def inject(self, model: nn.Module) -> None:
     self._error_map_allocate(model)
     if self.error_model == 'random':
       for param_name, param in model.named_parameters():
         if param_name.split('.')[-1] in self.param_names:
-          error_mask = self._error_map[torch.randperm(self._error_map.numel(), device = self.device)][:param.numel()]
+          error_mask = self._error_map['universal'][torch.randperm(self._error_map.numel(), device = self.device)][:param.numel()]
           error_mask = error_mask.reshape_as(param)
           param.data = (param.view(torch.int) ^ error_mask).view(torch.float)
-    if self.error_model == 'stuck_at_fault':
+    elif self.error_model == 'stuck_at_fault':
+      for param_name, param in model.named_parameters():
+        if param_name in self.param_names:
+          error_mask = self._error_map[param_name]
+          param.data = (param.view(torch.int) ^ error_mask).view(torch.float)
 
-  def save_errormap(self, path, sparse = False) -> None:
-    maptensor = self._error_map.clone()  
+  def save_error_map(self, path, sparse = False) -> None:
+    error_maps = self._error_maps.copy()
     if self.device != torch.device('cpu'):
-      maptensor = maptensor.cpu()
+      error_maps = [error_map.cpu() for error_map in error_maps]
     if sparse == True:
-      maptensor = maptensor.to_sparse() 
-    torch.save(maptensor, path)
+      error_maps = [error_map.to_sparse()  for error_map in error_maps]
+    torch.save(error_maps, path)
   
-  def load_errormap(self, path, sparse = False) -> None:
-    maptensor = torch.load(path)
+  def load_error_map(self, path, sparse = False) -> None:
+    error_maps = torch.load(path)
     if self.device != torch.device('cpu'):
-      maptensor = maptensor.to(self.device)
+      error_maps = [error_map.to(self.device) for error_map in error_maps]
     if sparse == True:
-      maptensor = maptensor.to_dense()
-    self._error_map = maptensor.clone()
+      error_maps = [error_map.to_dense() for error_map in error_maps]
+    self._error_maps = error_maps.copy()

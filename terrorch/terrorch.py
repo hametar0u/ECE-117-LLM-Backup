6064f8c985b075111c09d24106dbe3a643f7988a
AikawaMafuyu <dma@villanova.edu>
2023-01-29 22:04:46 -0500

    Huge rework on error map generation

--- terrorch/terrorch.py
+++ terrorch/terrorch.py
@@ -4,9 +4,10 @@ import torch.nn as nn
 class Injector():
   valid_dtypes = [torch.float, ]
   valid_error_models = ['bit', 'value']
+  valid_error_types = ['random', 'stuck_at_fault']
 
   @classmethod
-  def _error_map_generate(injectee_shape, dtype_bitwidth, device, p) -> torch.Tensor:
+  def _error_map_generate(cls, injectee_shape, dtype_bitwidth, device, p) -> torch.Tensor:
     error_map = (2 * torch.ones((injectee_shape, dtype_bitwidth), dtype = torch.int, device = device)) ** torch.arange(0, dtype_bitwidth, dtype = torch.int, device = device).expand((injectee_shape, dtype_bitwidth))
     filter = nn.functional.dropout(torch.ones_like(error_map , dtype = torch.float, device = device), 1 - p)
     error_map  = (filter.int() * error_map).sum(dim = -1).int()
@@ -18,7 +19,8 @@ class Injector():
       param_names: list = ['weight'],
       device: torch.device = torch.device('cpu'),
       verbose: bool = False,
-      error_model = 'bit'
+      error_model = 'bit',
+      error_type = 'random',
 
       ) -> None:
 
@@ -28,6 +30,7 @@ class Injector():
     self.device = device
     self.verbose = verbose
     self.error_model = error_model
+    self.error_type = error_type
 
     self._argument_validate()
     self._dtype_bitwidth = torch.finfo(self.dtype).bits
@@ -37,22 +40,24 @@ class Injector():
     if self.p <= 0 or self.p >= 1:
       raise ValueError('Invalid probability of error injection.')
     if self.dtype not in Injector.valid_dtypes:
-      raise ValueError('Invalid data types.')
+      raise ValueError('Invalid data types. Currently support:', *Injector.valid_dtypes)
     if self.error_model not in Injector.valid_error_models:
-      raise ValueError('Unknown error model.')
+      raise ValueError('Unknown error model. Currently support:', *Injector.valid_error_models)
+    if self.error_type not in Injector.valid_error_types:
+      raise ValueError('Unknown error type. Currently support:', *Injector.valid_error_types)
   
   def _error_map_allocate(self, model: nn.Module) -> None:
-    if self.error_model == 'random':
+    if self.error_type == 'random':
       self._maxsize = 0
       for param_name, param in model.named_parameters():
         if param_name.split('.')[-1] in self.param_names:
-          if param.numel() * self._dtype_bitwidth > self.maxsize:
-            self.maxsize = param.numel() * self._dtype_bitwidth
+          if param.numel() * self._dtype_bitwidth > self._maxsize:
+            self._maxsize = param.numel() * self._dtype_bitwidth
 
-      injectee_shape = self.maxsize
+      injectee_shape = self._maxsize
       self._error_maps['universal'] = Injector._error_map_generate(injectee_shape, self._dtype_bitwidth, self.device, self.p)
 
-    elif self.error_model == 'stuck_at_fault':
+    elif self.error_type == 'stuck_at_fault':
       for param_name, param in model.named_parameters():
         if param_name.split('.')[-1] in self.param_names:
           injectee_shape = torch.zeros_like(param)
@@ -61,30 +66,35 @@ class Injector():
 
   def inject(self, model: nn.Module) -> None:
     self._error_map_allocate(model)
-    if self.error_model == 'random':
+    if self.error_type == 'random':
       for param_name, param in model.named_parameters():
         if param_name.split('.')[-1] in self.param_names:
-          error_mask = self._error_map['universal'][torch.randperm(self._error_map.numel(), device = self.device)][:param.numel()]
+          error_map = self._error_maps['universal']
+          error_mask = error_map[torch.randperm(error_map.numel(), device = self.device)][:param.numel()]
           error_mask = error_mask.reshape_as(param)
           param.data = (param.view(torch.int) ^ error_mask).view(torch.float)
-    elif self.error_model == 'stuck_at_fault':
+    elif self.error_type == 'stuck_at_fault':
       for param_name, param in model.named_parameters():
         if param_name in self.param_names:
-          error_mask = self._error_map[param_name]
+          error_mask = self._error_maps[param_name]
           param.data = (param.view(torch.int) ^ error_mask).view(torch.float)
 
   def save_error_map(self, path, sparse = False) -> None:
     error_maps = self._error_maps.copy()
-    if self.device != torch.device('cpu'):
-      error_maps = [error_map.cpu() for error_map in error_maps]
-    if sparse == True:
-      error_maps = [error_map.to_sparse()  for error_map in error_maps]
+    for _, v in error_maps.items():
+      if self.device != torch.device('cpu'):
+        v = v.cpu()
+      if sparse == True:
+        v = v.to_sparse()
     torch.save(error_maps, path)
+    del error_maps
   
   def load_error_map(self, path, sparse = False) -> None:
     error_maps = torch.load(path)
-    if self.device != torch.device('cpu'):
-      error_maps = [error_map.to(self.device) for error_map in error_maps]
-    if sparse == True:
-      error_maps = [error_map.to_dense() for error_map in error_maps]
+    for _, v in error_maps.items():
+      if self.device != torch.device('cpu'):
+        v = v.to(self.device)
+      if sparse == True:
+        v = v.to_dense()
     self._error_maps = error_maps.copy()
+    del error_maps

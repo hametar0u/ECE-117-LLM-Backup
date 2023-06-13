b22b42d209ea0589364628c4eeabbaa5e26b4019
AikawaMafuyu <dma@villanova.edu>
2023-06-13 08:54:33 -0400

    Fix file name issue

--- pytei/pytei.py
+++ pytei/pytei.py
@@ -123,8 +123,7 @@ class Injector():
                 error_mask = self._error_maps[param_name]
                 error_count_number += self._error_count[param_name].sum()
                 param_count_number += self._error_maps[param_name].numel()
-                param.data = (param.view(torch.int) ^
-                                error_mask).view(torch.float)
+                param.data = (param.view(torch.int) ^ error_mask).view(torch.float)
 
         if self.verbose == True:
             injected_params = self._error_maps.keys()
